cab4d076b8ba3db0acc90981fc1a883029c3b8ab
fajaragngn <fnugroho159@gmail.com>
2020-12-22 21:35:49 +0700

    update

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/style.css">
+    <link href="favicon.ico" rel="icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet">
@@ -26,13 +27,12 @@
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav ml-auto">
             <a class="nav-item nav-link btn btn-secondary text-white tombol" href="https://wa.me/6289637524919?" target="_blank">Pasang sekarang</a><br>
-            <a class="nav-item nav-link btn btn-secondary text-white tombol" href="login">Log In</a>
+            <a class="nav-item nav-link btn btn-secondary text-white tombol" href="login">Log In client</a>
           </div>
         </div>
       </div>
     </nav>
     <!-- akhir Navbar -->
-
 
     <!-- Jumbotron -->
     <div class="jumbotron jumbotron-fluid">
@@ -43,7 +43,10 @@
     </div>
     <!-- akhir Jumbotron -->
 
+    <!-- container -->
+    <div class="container">
 
+      
     <!-- container -->
     <div class="container">
       <!-- info panel -->
@@ -56,8 +59,8 @@
               <p>Lorem ipsum dolor sit amet.</p>
             </div>
             <div class="col-lg">
-              <img src="public/img/hires.png" alt="HiRes" class="img-fluid float-left">
-              <h4>High-Res</h4>
+              <img src="public/img/rocket.png" width="110px" alt="HiRes" class="img-fluid float-left">
+              <h4>Up to 2mbps</h4>
               <p>Lorem ipsum dolor sit amet.</p>
             </div>
             <div class="col-lg">
@@ -97,7 +100,7 @@
 <br>
 
 <div class="container" style="width: 400px" >
-<a class="nav-item nav-link btn btn-secondary text-white tombol" href="https://wa.me/6289637524919?" target="_blank">Pasang sekarang</a><br>
+<a class="nav-item nav-link btn btn-primary text-white tombol" href="https://wa.me/6289637524919?" target="_blank">Pasang sekarang</a><br>
 </div>
 <div class="row justify-content-center info-text">
   <div class="col-lg text-center">
@@ -105,12 +108,8 @@
   </div>
 </div>
 
-
-
-    
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
